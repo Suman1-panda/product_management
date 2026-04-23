0d109ae4337cf7fb99ac4ade847f425875f7506d
Suman1-panda <pandasuman959@gmail.com>
2026-04-23 22:17:26 +0530

    commit 3

---
+++ product.management/src/main/java/project/product/management/service/ProductServiceImplementation.java
@@ -0,0 +1,50 @@
+package project.product.management.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import project.product.management.dto.ProductRequestDto;
+import project.product.management.entity.Product;
+import project.product.management.repository.ProductRepository;
+
+@Service
+public class ProductServiceImplementation implements ProductService{
+	
+	ProductRepository repo;
+	public ProductServiceImplementation(ProductRepository repo) {
+		super();
+		this.repo = repo;
+	}
+	@Override
+	public String addProduct(ProductRequestDto prod) {
+		Product p = new Product();
+		p.setName(prod.getName());
+		p.setDescription(prod.getDescription());
+		p.setPrice(prod.getPrice());
+		p.setPhotoUrl(prod.getPhotoUrl());
+		repo.save(p);
+		return "Product added successfully!";
+	}
+	@Override
+	public String updateProduct(Product prod) {
+		repo.save(prod);
+		return "Product updated successfully!";
+	}
+	@Override
+	public String deleteProduct(Long prodId) {
+		repo.deleteById(prodId);
+		return "Product deleted successfully!";
+	}
+	@Override
+	public Product viewProduct(Long prodId) {
+		return repo.findById(prodId).get();
+	}
+	@Override
+	public List<Product> viewAllProducts() {
+		return repo.findAll();
+	}
+
+
+
+}
